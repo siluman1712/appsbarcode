dd1aa259c95ad49943233d4ea100993113ab4bb5
siluman1712 <ikhwan12.addinusurya@gmail.com>
2023-06-02 05:38:26 +0700

    02062023
    Ubah Config

--- config/psl-config.php
+++ config/psl-config.php
@@ -1,9 +1,9 @@
 <?php
 /** These are the database login details */ 
-define("HOST", "178.100.212.8");    // The host you want to connect to.
-define("USER", "bmnkanreg");    // The database username. 
-define("PASSWORD", "Kanreg12");  // The database password. 
-define("DATABASE", "mydbx");     // The database name.
+define("HOST", "10.100.3.142");    // The host you want to connect to.
+define("USER", "siluman");    // The database username. 
+define("PASSWORD", "FS@#RserP2v#@");  // The database password. 
+define("DATABASE", "bkn12_silum4n");     // The database name.
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
 define("SECURE", TRUE);    // FALSE for Dev, TRUE for Prod using https
